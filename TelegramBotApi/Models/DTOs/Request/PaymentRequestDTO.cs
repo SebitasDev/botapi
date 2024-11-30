1ba9b9cf04e1386aff43f573d994759700ab4261
SebitasDev <sramireg13@eafit.edu.co>
2024-11-29 22:35:45 -0500

    Delete TelegramBotApi directory

--- TelegramBotApi/Models/DTOs/Request/PaymentRequestDTO.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace TelegramBotApi.Models.DTOs.Request;
-
-public class PaymentRequestDTO
-{
-    public string PaymentId { get; set; }
-}
--- TelegramBotApi/Models/DTOs/Response/PaymentResponseDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TelegramBotApi.Models.DTOs.Response;
-
-public class PaymentResponseDTO
-{
-    public bool Status { get; set; }
-    public string PaymentDetail { get; set; }
-    public string PaymentMessage { get; set; }
-}
